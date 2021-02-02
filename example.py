179224c10c4d0bba64eae916b7fd563b2fed5ae0
Rachit Singh <ayush.rlb1999@gmail.com>
2021-02-02 10:35:09 +0530

    Added an input statement and a print statement

--- example.py
+++ example.py
@@ -1,6 +1,9 @@
 #! /usr/bin/python3
 
 def git_opeation():
- print("I am adding example.py file to the remote repository.")
+    print("I am adding example.py file to the remote repository.")
+    name = input("Enter a name: ")
+    print("Your name is ", name)
+    
 git_opeation()
 
