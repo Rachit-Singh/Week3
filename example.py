5352459451d684760c4f93ff11bfa036261d5661
Rachit Singh <ayush.rlb1999@gmail.com>
2021-02-02 10:38:42 +0530

    Added an input statement for age and wrote an if else statement to print if user is young or not

--- example.py
+++ example.py
@@ -3,7 +3,14 @@
 def git_opeation():
     print("I am adding example.py file to the remote repository.")
     name = input("Enter a name: ")
+    age = int(input())
+
     print("Your name is ", name)
+
+    if age < 18 :
+        print("You are not young")
+    else :
+        print("You are young")
     
 git_opeation()
 
